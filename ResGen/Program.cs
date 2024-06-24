931888bf1f9e05e212c9366df151de263dffe947
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2024-06-24 12:16:23 +0200

    Add files via upload

---
+++ ResGen/Program.cs
@@ -0,0 +1,131 @@
+﻿public struct Mode
+{
+    public int a;
+    public int b;
+    public ColorDepth c;
+    public Mode(int a,int b,ColorDepth c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+public enum ColorDepth
+    {
+        /// <summary>
+        /// 4 bits per pixel; 16 colors.
+        /// </summary>
+        ColorDepth4 = 4,
+
+        /// <summary>
+        /// 8 bits per pixel; 256 colors.
+        /// </summary>
+        ColorDepth8 = 8,   /* VGA 256 colors */
+
+        /// <summary>
+        /// 16 bits per pixel; 65535 colors.
+        /// </summary>
+        ColorDepth16 = 16,
+
+        /// <summary>
+        /// 24 bits per pixel; 16,777,216 colors.
+        /// </summary>
+        ColorDepth24 = 24,
+
+        /// <summary>
+        /// 32 bits per pixel; 16,777,216 of colors with transparency (alpha values).
+        /// </summary>
+        ColorDepth32 = 32
+    }
+class Program
+{
+
+    public static List<Mode> AvailableModes { get; } = new List<Mode>
+        {
+            /*  VmWare maybe supports 16 bit resolutions but CGS not yet (we should need to do RGB32->RGB16 conversion) */
+            /* 16-bit Depth Resolutions*/
+
+            /* SD Resolutions */
+            new Mode(320, 200, ColorDepth.ColorDepth16),
+            new Mode(320, 240, ColorDepth.ColorDepth16),
+            new Mode(640, 480, ColorDepth.ColorDepth16),
+            new Mode(720, 480, ColorDepth.ColorDepth16),
+            new Mode(800, 600, ColorDepth.ColorDepth16),
+            new Mode(1024, 768, ColorDepth.ColorDepth16),
+            new Mode(1152, 768, ColorDepth.ColorDepth16),
+
+            /* Old HD-Ready Resolutions */
+            new Mode(1280, 720, ColorDepth.ColorDepth16),
+            new Mode(1280, 768, ColorDepth.ColorDepth16),
+            new Mode(1280, 800, ColorDepth.ColorDepth16),  // WXGA
+            new Mode(1280, 1024, ColorDepth.ColorDepth16), // SXGA
+
+            /* Better HD-Ready Resolutions */
+            new Mode(1360, 768, ColorDepth.ColorDepth16),
+            new Mode(1366, 768, ColorDepth.ColorDepth16),  // Original Laptop Resolution
+            new Mode(1440, 900, ColorDepth.ColorDepth16),  // WXGA+
+            new Mode(1400, 1050, ColorDepth.ColorDepth16), // SXGA+
+            new Mode(1600, 1200, ColorDepth.ColorDepth16), // UXGA
+            new Mode(1680, 1050, ColorDepth.ColorDepth16), // WXGA++
+
+            /* HDTV Resolutions */
+            new Mode(1920, 1080, ColorDepth.ColorDepth16),
+            new Mode(1920, 1200, ColorDepth.ColorDepth16), // WUXGA
+
+            /* 2K Resolutions */
+            new Mode(2048, 1536, ColorDepth.ColorDepth16), // QXGA
+            new Mode(2560, 1080, ColorDepth.ColorDepth16), // UW-UXGA
+            new Mode(2560, 1600, ColorDepth.ColorDepth16), // WQXGA
+            new Mode(2560, 2048, ColorDepth.ColorDepth16), // QXGA+
+            new Mode(3200, 2048, ColorDepth.ColorDepth16), // WQXGA+
+            new Mode(3200, 2400, ColorDepth.ColorDepth16), // QUXGA
+            new Mode(3840, 2400, ColorDepth.ColorDepth16), // WQUXGA
+            /* 32-bit Depth Resolutions*/
+            /* SD Resolutions */
+                new Mode(320, 200, ColorDepth.ColorDepth32),
+                new Mode(320, 240, ColorDepth.ColorDepth32),
+                new Mode(640, 480, ColorDepth.ColorDepth32),
+                new Mode(720, 480, ColorDepth.ColorDepth32),
+                new Mode(800, 600, ColorDepth.ColorDepth32),
+                new Mode(1024, 768, ColorDepth.ColorDepth32),
+                new Mode(1152, 768, ColorDepth.ColorDepth32),
+
+                /* Old HD-Ready Resolutions */
+                new Mode(1280, 720, ColorDepth.ColorDepth32),
+                new Mode(1280, 768, ColorDepth.ColorDepth32),
+                new Mode(1280, 800, ColorDepth.ColorDepth32),  // WXGA
+                new Mode(1280, 1024, ColorDepth.ColorDepth32), // SXGA
+
+                /* Better HD-Ready Resolutions */
+                new Mode(1360, 768, ColorDepth.ColorDepth32),
+                //new Mode(1366, 768, ColorDepth.ColorDepth32),  // Original Laptop Resolution - this one is for some reason broken in vmware
+                new Mode(1440, 900, ColorDepth.ColorDepth32),  // WXGA+
+                new Mode(1400, 1050, ColorDepth.ColorDepth32), // SXGA+
+                new Mode(1600, 1200, ColorDepth.ColorDepth32), // UXGA
+                new Mode(1680, 1050, ColorDepth.ColorDepth32), // WXGA++
+
+                /* HDTV Resolutions */
+                new Mode(1920, 1080, ColorDepth.ColorDepth32),
+                new Mode(1920, 1200, ColorDepth.ColorDepth32), // WUXGA
+
+                /* 2K Resolutions */
+                new Mode(2048, 1536, ColorDepth.ColorDepth32), // QXGA
+                new Mode(2560, 1080, ColorDepth.ColorDepth32), // UW-UXGA
+                new Mode(2560, 1600, ColorDepth.ColorDepth32), // WQXGA
+                new Mode(2560, 2048, ColorDepth.ColorDepth32), // QXGA+
+                new Mode(3200, 2048, ColorDepth.ColorDepth32), // WQXGA+
+                new Mode(3200, 2400, ColorDepth.ColorDepth32), // QUXGA
+                new Mode(3840, 2400, ColorDepth.ColorDepth32), // WQUXGA
+        };
+
+    public static void Main()
+    {
+        string buff="";
+        foreach(Mode m in AvailableModes)
+        {
+            Console.WriteLine($"_{m.a}x{m.b} = {m.a} * {m.b}");
+            buff += $"_{m.a}x{m.b} = {m.a} * {m.b},\n";
+        }
+        File.WriteAllText("modes.txt",buff);
+    }
+}
